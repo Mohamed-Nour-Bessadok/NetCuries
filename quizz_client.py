b8ee927f373e0a4d8004249aad25b6386487e910
Mohamed Nour Bessadok <124530944+Mohamed-Nour-Bessadok@users.noreply.github.com>
2023-11-10 00:54:33 +0100

    Fixed Threading

--- quizz_client.py
+++ quizz_client.py
@@ -1,38 +1,64 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 28 17:45:11 2023
+import socket
+import time 
+import random
+import threading 
 
-@author: inesb
-"""
+users = {}
+questions = [
+    {'question': 'What is the capital of France?',
+     'options': ['A. London', 'B. Berlin', 'C. Paris', 'D. Madrid'],
+     'correct_answer': 'C'},
+    
+    {'question': 'What is the capital of Tunisia?',
+     'options': ['A. London', 'B. Tunis', 'C. Berlin', 'D. Madrid'],
+     'correct_answer': 'B'},
+    
+    {'question': 'What is the capital of UK?',
+     'options': ['A. London', 'B. Berlin', 'C. Paris', 'D. Madrid'],
+     'correct_answer': 'A'},
+]
+scores = {}
 
-from socket import *
-import sys
-import select
+def handle_client(client_socket,username):
+    try:
+        while questions:
+            current_question=random.choice(questions)
+            question_text=current_question['question']
+            options="\n".join(current_question['options'])
+            client_socket.send(f'Question: {question_text}\n{options}\n'.encode())
+            client_answer = client_socket.recv(1024).decode().strip().upper()
+            print(client_answer)
+            if client_answer != "":
+                correct_answer = current_question['correct_answer']
+                if correct_answer == client_answer:
+                    scores[username]=scores.get(username,0)+1
+                    client_socket.send('Correct! \n'.encode())
+                else:
+                     client_socket.send('Wrong Answer! \n The correct answer is : {correct_answer}\n'.encode())
+            else:
+                client_socket.send("time over! better luck next time.\n".encode())
 
-server_name = "192.168.50.147"
-server_port = 12222 #enter the server port
+            client_socket.send(f'Score: {scores.get(username, 0)}\n'.encode())
+            questions.remove(current_question)
+           
+    except Exception as e: 
+        print(f'Client {username} disconnected: {e}')
+        client_socket.close()
 
-client_socket = socket(AF_INET,SOCK_STREAM)
 
-client_socket.connect((server_name,server_port))
-
-print("Connection to server successful")
-
-#Send the unique username
-username = input("username:")
-client_socket.send(username.encode())
-
+host = ''
+port = 12222
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #TCP 
+sock.bind((host,port))
+sock.listen(3)
+print(f"UDP server is listening on {host}:{port}")
 
 while True:
-    # Receive the questions and options from the server
-    questions_options = client_socket.recv(1024)
-    print(questions_options.decode())  
     
-    answer = input()
-    # Receive the score from the server
-    score = client_socket.recv(1024)
-    print("The score", score.decode())  # Decode the received score before printing
-    
+    client,address =sock.accept()
+    username=client.recv(1024).decode()
+    users[username]=client
 
-client_socket.close()
-
+    print(f'Accepted connection from {address[0]}:{address[1]} as {username}')
+    client_handler=threading.Thread(target=handle_client,args=(client, username))
+    client_handler.start()
